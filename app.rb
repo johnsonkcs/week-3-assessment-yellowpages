33546418111ddb5f9ee7c61c9c07a42d20d23706
Next <nextacademy@email.com>
2016-03-18 00:46:58 -0700

    super front ended methodized

--- app.rb
+++ app.rb
@@ -10,22 +10,17 @@ require 'byebug'
 # List.create(name: "Brad Pitt", phone: "0123779886", address: "Sunnyvilla 1762, Off Hello Road")
 # List.create(name: "Ali Baba", phone: "01988888888", address: "China Jungle Villa, 2278139")
 
-
-def add(name, phone, address)
-
-      List.create(name: name, phone: phone, address: address)
-
-      puts "Congrats! Name: #{name}, Phone: #{phone}, Address: #{address} has been added!"
-
-end
-
-# add("Ray Ban", "0123770665", "USS Enterprise, Galaxy")
-
-def list
+def clear_screen
 	puts "\e[2J" 
     print "\e[H"
+end
 
-    n = 0
+def blank_line
+	puts ""
+end
+
+def trademark_startup_screen
+	n = 0
     g = 20
 
     while g >= 0
@@ -43,27 +38,81 @@ def list
 	sleep(0.2)
     end
 
-    sleep(1)
+    sleep(2)
+end
 
-    puts "\e[2J" 
-    print "\e[H"
-
-    puts ""
+def list_header
+	blank_line
     puts "============================================================================================================".ljust(180)
     puts "                        THE BEST YELLOW PAGES IN THE UNIVERSE! [v.2.4] ZBAR ".ljust(180)
     puts "============================================================================================================".ljust(180)
-    puts ""
+    blank_line
+end
+
+
+# def add(name, phone, address)
+
+#       List.create(name: name, phone: phone, address: address)
+
+#       puts "Congrats! Name: #{name}, Phone: #{phone}, Address: #{address} has been added!"
+
+# end
+
+# add("Ray Ban", "0123770665", "USS Enterprise, Galaxy")
+def normal_list
+
+	blank_line
+	list_header
+
 
 	everyone = List.all
 
-	    puts "No.".ljust(4) + "Name".ljust(20) +      "Phone".ljust(16)   +     "Address".ljust(30)
-	    puts "===".ljust(4) + "====".ljust(20) +      "=====".ljust(16)   +     "==========".ljust(30) 
-	    puts ""
+	puts "No.".ljust(4) + "Name".ljust(20) +      "Phone".ljust(16)   +     "Address".ljust(30)
+	puts "===".ljust(4) + "====".ljust(20) +      "=====".ljust(16)   +     "==========".ljust(30) 
+	blank_line
+	    
 
 	everyone.each do |row|
 		puts "#{row.id}.".ljust(4) + "#{row.name}".ljust(20) +  "#{row.phone}".ljust(16) +  "#{row.address}".ljust(30)
 	end
-	puts ""
+
+end
+
+def list
+	clear_screen
+	blank_line
+	trademark_startup_screen
+	clear_screen
+
+	normal_list
+
+	# everyone = List.all
+
+	#     puts "No.".ljust(4) + "Name".ljust(20) +      "Phone".ljust(16)   +     "Address".ljust(30)
+	#     puts "===".ljust(4) + "====".ljust(20) +      "=====".ljust(16)   +     "==========".ljust(30) 
+	#     blank_line
+	    
+
+	# everyone.each do |row|
+	# 	puts "#{row.id}.".ljust(4) + "#{row.name}".ljust(20) +  "#{row.phone}".ljust(16) +  "#{row.address}".ljust(30)
+	# end
+	blank_line
+end
+
+def add(name, phone, address)
+
+      List.create(name: name, phone: phone, address: address)
+
+      clear_screen
+      blank_line
+      puts "Congrats! Name: #{name}, Phone: #{phone}, Address: #{address} has been added!".center(180)
+      blank_line
+      puts "HERE IS YOUR UPDATED LIST!".center(180)
+      sleep(3)
+      clear_screen
+      normal_list
+      blank_line
+
 end
 
 
@@ -76,10 +125,15 @@ def update(id, name, phone, address)
   end
 
   if (list_of_ids.include?id) == false
-  	puts "\e[2J" 
-    print "\e[H"
+
+  	clear_screen
   	puts "Sorry man, there is no such id/person! Did you remember correctly? Please try again..".center(180)
-    puts ""
+    sleep(3)
+    clear_screen
+    normal_list
+    blank_line
+   
+    
   else
   	list_of_people = List.where(id: id)
   	target = list_of_people[0]
@@ -88,10 +142,16 @@ def update(id, name, phone, address)
   	target.address = address
 
   	target.save
-  	puts "\e[2J" 
-    print "\e[H"
+  
+  	clear_screen
   	puts "Yayy! The new data has been updated!".center(180)
-  	puts ""
+  	blank_line
+  	puts "HERE IS YOUR NEW CONTACT LIST!".center(180)
+  	sleep(3)
+  	clear_screen
+  	normal_list
+  	blank_line
+ 
   end
 
 end
@@ -112,20 +172,28 @@ def delete(id)
     end
 
     if (list_of_ids.include?id) == false
-    puts "\e[2J" 
-    print "\e[H"
+    clear_screen
+   
   	puts "Sorry man, there is no such id/person! Did you remember correctly? Please try again..".center(180)
-    puts ""
+    blank_line
+    puts "Your list still looks like this! Woohoo!".center(180)
+    sleep(3)
+    clear_screen
+    normal_list
+    blank_line
     else
   	list_of_people = List.where(id: id)
   	target = list_of_people[0]
   	target.destroy
-
-
-    puts "\e[2J" 
-    print "\e[H"
+    
+    clear_screen
   	puts "Muahahhahaahha!! AHHAHA! We've found that person and BLOWN that person up!".center(180)
-  	puts ""
+  	blank_line
+  	puts "THIS IS YOUR UPDATED CONTACT LIST WITH THAT PERSON DEAD! MUAHAHAHHAHA!".center(180)
+  	sleep(3)
+  	clear_screen
+  	normal_list
+  	blank_line
     end 
 end
 
